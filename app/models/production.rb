73bbc0471fec03a5c033009ee99a530c6bad96f7
Daniel Meißner <meise@koeln.ccc.de>
2015-08-06 22:31:57 +0200

    paperclip: try to fix permissions problem

--- app/models/production.rb
+++ app/models/production.rb
@@ -6,7 +6,9 @@ class Production < ActiveRecord::Base
   validates :description, presence: true
   validates :user, presence: true
 
-  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :logo, styles: { medium: "300x300>", thumb: "100x100>" },
+                    path:":rails_root/public/system/:attachment/:id_partition_:style_:filename",
+                    url: "/system/:attachment/:id_partition_:style_:filename"
   validates_attachment :logo,
     content_type: { content_type: ["image/jpeg", "image/gif", "image/png"] },
     size: { in: 0..512.kilobytes }
